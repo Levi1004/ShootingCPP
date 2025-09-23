99b71e5de8f922faf90542ad23e03c13ccac58fe
Levi1004 <rmsdud0140@naver.com>
2025-09-23 13:37:14 +0900

    git init

---
+++ Source/ShootingCPP/ShootingGamemodeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShootingGamemodeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTINGCPP_API AShootingGamemodeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
